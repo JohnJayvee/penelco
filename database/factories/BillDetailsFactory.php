5b3da4e9b7dcaeb91da44fa961be67cae70f6f6c
maganasamuel <maganasamuel001@gmail.com>
2021-04-06 14:44:20 +0800

    Created factory for bill details

---
+++ database/factories/BillDetailsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Laravel\Models\BillDetails;
+use Faker\Generator as Faker;
+
+$factory->define(BillDetails::class, function (Faker $faker) {
+    return [
+
+    ];
+});
